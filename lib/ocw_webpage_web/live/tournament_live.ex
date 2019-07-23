b7b375b3d588359df51963a49597a72300cd3033
Kamil <paryzowy@gmail.com>
2019-07-24 00:04:22 +0200

    Add records

--- lib/ocw_webpage_web/live/tournament_live.ex
+++ lib/ocw_webpage_web/live/tournament_live.ex
@@ -9,22 +9,21 @@ defmodule OcwWebpageWeb.TournamentLive do
       <div class="row header">
         OCW &lt; back to website
       </div>
-      <div class="row">
+      <div class="row body">
         <div class="col s9 board">
-          {currentRound && (
-            <MainBoardTop data={currentRound} />
-          )}
-          <MainBoardRecords data={MainBoardRecordStub} />
-          {results && (
-            <MainBoardTable data={results} />
+          <%= OcwWebpageWeb.PageView.render("main_board_top.html", assigns) %>
+          <%= OcwWebpageWeb.PageView.render("main_board_records.html", assigns.records) %>
+          <MainBoardRecords data=%{MainBoardRecordStub} />
+          %{results && (
+            <MainBoardTable data=%{results} />
           )}
         </div>
         <div class="col s3 sidebar">
-          {tournamentName && (
-            <MainSidebarCard name={tournamentName} />
+          %{tournamentName && (
+            <MainSidebarCard name=%{tournamentName} />
           )}
-          {eventsNamesWithRoundNames && (
-            <MainSidebarList tournamentName={tournamentName} data={eventsNamesWithRoundNames}/>
+          %{eventsNamesWithRoundNames && (
+            <MainSidebarList tournamentName=%{tournamentName} data=%{eventsNamesWithRoundNames}/>
           )}
         </div>
       </div>
@@ -43,7 +42,60 @@ defmodule OcwWebpageWeb.TournamentLive do
         socket
       ) do
     round = Services.Tournaments.fetch_round(tournament_name, event_name, round_name)
+    records = stub_records()
+    event_with_rounds = Services.Tournaments.fetch_event_with_rounds(tournament_name)
 
-    {:ok, assign(socket, :round, round)}
+    new_socket =
+      socket
+      |> assign(:round, round)
+      |> assign(:records, records)
+      |> assign(:event_with_rounds, event_with_rounds)
+
+    {:ok, new_socket}
+  end
+
+  defp stub_records() do
+    %{
+      wr: %{
+        single: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        },
+        average: %{
+          person: "Rafał Studnicki",
+          time: "00:04.70"
+        }
+      },
+      cr: %{
+        single: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        },
+        average: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        }
+      },
+      nr: %{
+        single: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        },
+        average: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        }
+      },
+      cb: %{
+        single: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        },
+        average: %{
+          person: "Rafał Studnicki",
+          time: "00:05.70"
+        }
+      }
+    }
   end
 end
